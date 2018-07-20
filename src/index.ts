8ca1a149dba76ba625dbccaae325032791d56c11
Michael Cabana <m.cabana@ieg-america.com>
2018-07-20 14:51:58 -0400

    base set up

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,62 @@ import { User } from './entity/User';
 
 createConnection().then(async connection => {
 
+    const app = express();
+    app.use(bodyParser.json());
+
+
+    // register routes for the api
+    Routes.UserRoutes.forEach((route) => {
+        app[route.method](route.path, (req: Request, res: Response, next: NextFunction) => {
+
+            const result = (new (route.controller))[route.action](req, res, next);
+
+            if (result instanceof Promise) {
+                result
+                    .then((result) => {
+                        return result !== null && result !== undefined ? res.json(result) : undefined;
+                    })
+                    .catch((err) => {
+                        const query = err.query;
+                        res.status(500).json({
+                            name: err.name,
+                            message: err.message,
+                            data: {
+                                query
+                            }
+                        });
+                    });
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+
+        });
+    });
+
+    // set up express app here
+    // 
+    // 
+    // =======================
+
+    app.listen(3000);
+
     console.log('Seeding database...');
 
-    const user = new User();
-    user.firstName = 'Timber';
-    user.lastName = 'Saw';
-    user.email = 'user@example.com';
-    user.isLocked = true;
-    user.creationDate = new Date();
-    user.password = '!Password001'
-    await connection.manager.save(user);
 
+    const existingUser = await connection.manager.find(User, { email: 'user@example.com' });
+    if (!existingUser) {
+        await connection.manager.save(connection.manager.create(User, {
+            firstName: 'Timber',
+            lastName: 'Saw',
+            email: 'user@example.com',
+            isLocked: true,
+            password: '!Password001',
+            creationDate: new Date()
+        }));
+    }
 
-    console.log('Loading users from the database...');
-    const users = await connection.manager.find(User);
-    console.log('Loaded users: ', users);
-
-
+    // console.log('Loading users from the database...');
+    // const users = await connection.manager.find(User);
+    // console.log('Loaded users: ', users);
 
 }).catch(error => console.log(error));
--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,2 +1,35 @@
-import { Router } from 'express';
+import { UserController } from '../controller/UserController';
+
+export const UserRoutes = [
+    {
+        path: '/users',
+        method: 'get',
+        controller: UserController,
+        action: 'getAll'
+    },
+    {
+        path: '/users/:id',
+        method: 'get',
+        controller: UserController,
+        action: 'get'
+    },
+    {
+        path: '/users',
+        method: 'post',
+        controller: UserController,
+        action: 'post'
+    },
+    {
+        path: '/users/:id',
+        method: 'put',
+        controller: UserController,
+        action: 'put'
+    },
+    {
+        path: '/users',
+        method: 'delete',
+        controller: UserController,
+        action: 'delete'
+    }
+];
 
